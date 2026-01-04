82d10bac273e7013d2fd6be3876737ea46c95096
jjw0609 <jjw_0609@naver.com>
2026-01-04 22:14:56 +0900

    질문&답변 실습

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-print("Hello, Python!")
+from openai import OpenAI
+
+api_key = ''
+client = OpenAI(api_key=api_key)
+
+
+res1 = client.responses.create(
+    model="gpt-4o-mini",
+    input="프랑스의 수도는?",
+    store=True
+)
+
+print(res1.output_text)
+
+res2 = client.responses.creat(
+    model="gpt-4o-mini",
+    input="프랑스의 인구는?",
+    previous_response_id=res1.id,
+    store=True
+)
+
+print(res2.output_text)
+
+
+answer = client.responses.create(
+    model="gpt-4o-mini",
+    input="지금 프랑스 대통령이 누구야?",
+    tools=[{"type": "web_search_preview"}]
+)
+
+print(answer.output_text)
